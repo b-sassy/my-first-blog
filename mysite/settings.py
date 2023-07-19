f622273ec84e8f2328b3849281d973c6c1801efe
Your Name <you@example.com>
2023-07-19 13:51:56 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#!+c+u)$nxxi34#yr$ji2l+s=030f_=%ah2@eo)t@a4xw*77%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
